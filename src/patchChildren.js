b00ac5382d876c3466871c3af11862d5f106b66a
ziming <guoaihua@geetest.com>
2021-04-13 22:58:47 +0800

    feat: finish patchChildren

--- src/patchChildren.js
+++ src/patchChildren.js
@@ -1,4 +1,5 @@
 import patch from './patch';
+import createElm from './createElement';
 import patchVnode from './patchVnode';
 
 function sameVnode(oldVnode, newVnode){
@@ -24,10 +25,21 @@ export default function patchChildren(parent, oldch, newch){
     var oldEndIdx = oldch.length - 1;
     var oldEndVnode = oldch[oldEndIdx];
 
-    var count = 0;
+    let keymap = null;
+
     //开始遍历,结束条件为新后小于等于新前 或者旧后小于等于新前
-    while((( newStartIdx <= newEndIdx)|| ( oldStartIdx <= oldEndIdx) )&& count<20 ){
-        if(sameVnode(oldStartVnode, newStartVnode)){
+    while(( newStartIdx <= newEndIdx)&&( oldStartIdx <= oldEndIdx) ){
+        // 跳过已经处理完的节点
+
+        if(oldStartVnode === null || oldch[oldStartIdx] === undefined){
+            oldStartVnode = oldch[++oldStartIdx];
+        }else if(oldEndVnode === null || oldch[oldEndIdx] === undefined){
+            oldEndVnode = oldch[--oldEndIdx];
+        }else if(newStartVnode === null || newch[newStartIdx] === undefined){
+            newStartVnode = newch[++newStartIdx];
+        }else if(newEndVnode === null || newch[newEndIdx] === undefined){
+            newEndVnode = newch[--newEndIdx];
+        }else if(sameVnode(oldStartVnode, newStartVnode)){
             console.log("@1 命中新前与旧前");
             patchVnode(oldStartVnode, newStartVnode);
             oldStartVnode = oldch[++oldStartIdx];
@@ -55,8 +67,52 @@ export default function patchChildren(parent, oldch, newch){
             oldEndVnode = oldch[--oldEndIdx];
             newStartVnode = newch[++newStartIdx];
 
-        }
+        }else {
+            // 都没有命中就开始以newVnode的顺序在旧的vnode查找index
 
-        count++;
+            if(!keymap){
+                keymap = {};
+                for(let i = 0; i < oldch.length; i++){
+                    let key = oldch[i].key;
+                    if(key){
+                        
+                         // 为旧的vnode缓存key和index
+                        keymap[key] = i;
+                    }
+                }
+            }
+
+            var idxInOld = keymap[newStartVnode.key];
+            if(!idxInOld){
+                // idxInOld 不存在，说明是一个全新的节点
+                parent.insertBefore(createElm(newStartVnode), oldStartVnode.elm);
+            }else {
+                // 如果找到了，说明是一个移动操作
+                const elmToMove = oldch[idxInOld];
+                patchVnode(elmToMove, newStartVnode);
+
+                oldch[idxInOld] = undefined;
+                parent.insertBefore(elmToMove.elm, oldStartVnode.elm);
+            }
+
+            // newStartVnode 节点在下移
+            newStartVnode = newch[++newStartIdx];
+        }
+    }
+
+    // 处理剩下的情况
+
+    // 旧节点先处理完的，说明新节点还有需要新增的
+    if(newStartIdx <= newEndIdx){
+            for(let i = newStartIdx; i <=newEndIdx; i++){
+                parent.insertBefore(createElm(newch[i]), oldch[oldStartIdx].elm);
+         }
+    }else {
+        // 新节点先处理完，说明还有未删除的项
+        for(let i = oldStartIdx; i <= oldEndIdx; i++){
+            if(oldch[i]){
+                parent.removeChild(oldch[i].elm);
+            }
+        }
     }
 }
